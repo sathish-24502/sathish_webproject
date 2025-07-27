b1f0bf747e146fc313c22b8ddd920e250940dca2
sathish-24502 <sathisheswari24@gmail.com>
2025-07-28 00:24:28 +0530

    update my files

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-// import Skills from "./components/Technologies";
 import SideNavbar from "./components/SideNavbar";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <SideNavbar/>
-      {/* <Skills /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
